4d1027361d2edafe7496f350a876a7b9c3504bd3
Minreng Wu <wumrwds@gmail.com>
2019-11-11 22:22:10 -0600

    feat: added Swagger documentation support

--- project_1/blog-backend/src/main/java/edu/tamu/wumrwds/database/controller/UserController.java
+++ project_1/blog-backend/src/main/java/edu/tamu/wumrwds/database/controller/UserController.java
@@ -6,6 +6,10 @@ import edu.tamu.wumrwds.database.entity.User;
 import edu.tamu.wumrwds.database.entity.ext.UserExt;
 import edu.tamu.wumrwds.database.entity.vo.Result;
 import edu.tamu.wumrwds.database.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +17,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+
 @RestController
 @RequestMapping("user")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Api(tags = "User API")
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -25,9 +33,13 @@ public class UserController {
     @Value("${project.version}")
     private String version;
 
-    @GetMapping(value = "/userList", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Result<PageInfo<? extends User>> getUserList(@RequestParam(name = "username", required = false) String username,
+    @ApiOperation(value = "Retrieves users by querying with specific keywords")
+    @ApiResponses(value = { @ApiResponse(code = SC_OK, message = "ok"),
+            @ApiResponse(code = SC_INTERNAL_SERVER_ERROR, message = "An unexpected error occurred")
+    })
+    public Result<PageInfo<? extends User>> getUsers(@RequestParam(name = "username", required = false) String username,
                                                         @RequestParam(name = "roleId", required = false) Integer roleId) {
 
         try {
